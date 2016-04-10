27f31c501d5f96065304e25c27c0f272b8f67351
adjivas <adjivas@users.noreply.github.com>
2016-04-10 18:20:16 +0200

    exclude comment

--- src/main.lisp
+++ src/main.lisp
@@ -1,6 +1,6 @@
 (defun read_dimension (in)
   "Function that read the taquin dimension
-   @args: open
+   @args: fd
    @return: (int)"
   (let ((line (read-line in)))
     (when line
@@ -8,10 +8,20 @@
 	(read_dimension in)
 	(parse-integer line)))))
 
+(defun read_board (in)
+  "Function that read the taquin board
+   @args: in
+   @return: (list int)"
+  (loop for line = (read-line in nil)
+    while line do
+      (let ((index (position #\# line)))
+	(format t "~a~%" (subseq line 0 index)))))
+
 (defun main ()
-  (let ((file (open "inputs/solvable_three.npuzzle" :if-does-not-exist nil)))
+  (let ((file (open "inputs/solvable_comment_four.npuzzle" :if-does-not-exist nil)))
     (when file
       (format t "~d~%" (read_dimension file))
+      (read_board file)
       (close file))))
 
 (main)
