420eaf0beb882b654ed51278c5858c5e5b645556
adjivas <adjivas@users.noreply.github.com>
2016-04-11 05:00:31 +0200

    first try with list

--- src/main.lisp
+++ src/main.lisp
@@ -8,32 +8,38 @@
 	(read_dimension in)
 	(parse-integer line)))))
 
-(defun parse_line (line iter)
+(defun parse_line (result line iter)
   "Function that parse the board line
    @args: string int
    @return: (list int)"
-  (format t "~d~%" iter)
-  (or (> iter 0)
-    ()
+  (if (> iter 1)
     (let ((index (position #\Space line)))
-      (format t "~d~a~%" index (subseq line 0 index)))))
+      (if (> index 0)
+        (parse_line (push (parse-integer (subseq line 0 index)) result) (subseq line index) (- iter 1))
+        (parse_line result (subseq line 1) iter)))
+    (push (parse-integer line) result)))
 
 (defun read_board (iter in)
   "Function that read the taquin board
    @args: int fd
    @return: (list int)"
-  (loop for line = (read-line in nil)
-    while line do
-      (let ((index (position #\# line)))
-        (parse_line (subseq line 0 index) iter))))
-      
+
+  (let ((cells (make-list (* iter iter))))
+    (loop for line = (read-line in nil)
+      while line do
+        (let ((index (position #\# line)))
+          (push (parse_line (make-list iter) (subseq line 0 index) iter) cells)))
+    cells))
+
+    ;(format t "cells: ~d~%" (parse_line (make-list iter) (subseq line 0 index) iter))
+    ;(cons (parse_line (make-list iter) (subseq line 0 index) iter)  cells))
+
 (defun main ()
   (let ((file (open "inputs/solvable_comment_four.npuzzle" :if-does-not-exist nil)))
     (when file
       (let ((lenght (read_dimension file)))
-	(format t "~d~%" lenght)
-	(read_board lenght file)
-      )
+	      (format t "dimension: ~d~%" lenght)
+        (format t "cells: ~d~%" (read_board lenght file)))
       (close file))))
 
 (main)
