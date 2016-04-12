c2e22a6bd3d4967a6181659937be49a4129907af
francois LE CORRE <lecorre.f@gmail.com>
2016-04-12 20:13:23 +0200

    variable goal-state/ put next moves in *open-set*

--- src/astar.lisp
+++ src/astar.lisp
@@ -1,98 +1,37 @@
-;struct: bord to get tile with position, x and y to get positions with tiles
-(defstruct (puzzle (:conc-name p-))
-  board x y)
+(load "puzzle.lisp")
+(defparameter *visited* (make-hash-table))
+(defparameter *came-from* (make-hash-table))
+(defvar *open-set* '())
+(defvar *closed-set* '())
+(defvar *goal* nil)
 
-(defun init-puzzle (size)
-  "Function that take a size to initialize the puzzle struct arrays
-  @args: int
-  @return: puzzle"
-  (make-puzzle
-    :board (make-array (list size size) :element-type 'integer)
-    :x (make-array (* size size) :element-type 'integer)
-    :y (make-array (* size size) :element-type 'integer)))
-
-;maybe improve this later?
-(defun copy-array (array)
- (let ((dim (array-dimensions array)))
-   (adjust-array
-    (make-array dim :displaced-to array)
-    dim)))
-
-(defun copy-puzzle (p)
-  (make-puzzle
-    :board (copy-array (p-board p))
-    :x (copy-array (p-x p))
-    :y (copy-array (p-y p))))
-
-;getters
-(defun get-tile (p x y)
-  (aref (p-board p) x y))
-
-(defun get-x (p tile)
-  (aref (p-x p) tile))
-
-(defun get-y (p tile)
-  (aref (p-y p) tile))
-
-;setters
-(defun set-tile (p tile x y)
-  (setf (aref (p-board p) x y) tile))
-
-(defun set-x (p tile x)
-  (setf (aref (p-x p) tile) x))
-
-(defun set-y (p tile y)
-  (setf (aref (p-y p) tile) y))
-
-(defun set-coord (p tile x y)
-  (set-x p tile x)
-  (set-y p tile y))
-
-(defun swap-tiles (p x1 y1 x2 y2)
-  (let* ((new (copy-puzzle p))
-         (tile (get-tile new x2 y2)))
-    (set-tile new 0 x2 y2)
-    (set-tile new tile x1 y1)
-    (set-coord new 0 x2 y2)
-    (set-coord new tile x1 y1)
-    (return-from swap-tiles new)))
-
-;return a list of all possible puzzle permutation
-(defun permutation-list (p size)
-  (let ((x (get-x p 0)) (y (get-y p 0)) (lst '()))
-    (and (> x 0) (setq lst(cons (swap-tiles p x y (- x 1) y) lst)))
-    (and (< x size) (setq lst (cons (swap-tiles p x y (+ x 1) y) lst)))
-    (and (> y 0) (setq lst (cons (swap-tiles p x y x (- y 1)) lst)))
-    (and (< y size) (setq lst (cons (swap-tiles p x y x (+ y 1)) lst)))
-    lst)
-  )
-
-;test if 2 puzzles are equal
-(defun test-eq (p1 p2 size)
-  (loop for n below size
-        if (not (and (= (get-x p1 n) (get-x p2 n))
-                     (= (get-y p1 n) (get-y p2 n))))
-        do (return nil)
-        finally (return t)))
-
-(defun manhattan (p1 p2 size)
+(defun manhattan (p size)
   (loop for n from 1 below size
         and ret = 0 then (+ ret
-                            (abs (- (get-x p1 n) (get-x p2 n)))
-                            (abs (- (get-y p1 n) (get-y p2 n))))
+                            (abs (- (get-x p n) (get-x *goal* n)))
+                            (abs (- (get-y p n) (get-y *goal* n))))
         finally (return ret)))
 
-;resolution: maybe set the heuristic later, in a closure, and maybe a print opt?
-(defun a-star (puzzle goal size)
-  (let ((linear-size (* size size)) (border (- size 1)))
-    (loop
+(defun get-next-moves (p size)
+  (mapc #'(lambda (x)
+            (or (gethash x *visited*)
+                (progn
+                  (setf (gethash x *visited*) x)
+                  (setf (gethash x *came-from*) p)
+                  (setq *open-set* (cons (cons (manhattan x (* size size)) x) *open-set*)))))
+        (permutation-list p (- size 1))))
 
-      do (progn (show-board (p-board p))
-               (format t "_________________________~%"))
-      until (test-eq puzzle goal linear-size)
-      )
-    )
-  )
+;resolution: maybe set the heuristic later, in a closure, and maybe a print opt?
+;(defun a-star (puzzle goal size)
+;  (let ((linear-size (* size size)) (border (- size 1)))
+;    (loop
+;
+;      do (progn (show-board (p-board p))
+;               (format t "_________________________~%"))
+;      until (test-eq puzzle goal linear-size)
+;      )
+;    )
+;  )
 
 ;make a structure form a list
 (defun list-to-puzzle (lst size)
@@ -113,7 +52,16 @@
         (format t "~%")))
 
 
-(let ((p1 (list-to-puzzle '( 1 2 3 8 4 7 6 5 0) 3))
+(let* ((p1 (list-to-puzzle '(1 2 3 8 4 7 6 5 0) 3))
       (p2 (list-to-puzzle '(1 2 3 8 0 4 7 6 5) 3)))
-    (a-star p1 p2 3)
+  (setf *goal* p2)
+  (setf (gethash p1 *visited*) p1)
+  (setf (gethash p2 *visited*) p2)
+  (print (gethash p1 *visited*))
+  (print (gethash p2 *visited*))
+  (get-next-moves p1 3)
+  (mapcar #'(lambda (x) (print x)) *open-set*)
+;    (show-board (p-board p1))
+;    (format t "_________________________~%")
+;    (show-board (p-board p2))
   )
