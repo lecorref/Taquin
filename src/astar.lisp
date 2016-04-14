8ba6ee063697fe5ddbc3cdfc8478eb72ce7e43bf
francois LE CORRE <lecorre.f@gmail.com>
2016-04-14 16:34:12 +0200

    passing heuristic as anonymous functions

--- src/astar.lisp
+++ src/astar.lisp
@@ -1,23 +1,17 @@
 (load "puzzle.lisp")
 (defparameter *visited* (make-hash-table :test 'equalp))
-(defparameter *came-from* (make-hash-table :test 'equalp))
 (defvar *open-set* '())
-(defvar *closed-set* '())
+(defvar *size* 0)
+(defvar *linear-size* 0)
 (defvar *goal* nil)
-
-(defun manhattan (p size)
-  (loop for n from 1 below size
-        and ret = 0 then (+ ret
-                            (abs (- (get-x p n) (get-x *goal* n)))
-                            (abs (- (get-y p n) (get-y *goal* n))))
-        finally (return ret)))
+(load "heuristics.lisp")
 
 ;print a nice board
 (defun show-board (board)
   (loop for i below (car (array-dimensions board)) do
         (loop for j below (cadr (array-dimensions board)) do
               (let ((cell (aref board j i)))
-                (format t "~a " cell)))
+                (format t "~v,'0d " (length (write-to-string *linear-size*)) cell)))
         (format t "~%")))
 
 (defun get-path (board)
@@ -25,7 +19,7 @@
         until (eql b 'end)
         collect b))
 
-(defun get-next-moves (p size)
+(defun get-next-moves (heuristic p)
   (let ((b (p-board p)))
   (mapc #'(lambda (x)
             (let ((xboard (p-board x)))
@@ -33,45 +27,29 @@
                 (progn
                   (setf (gethash xboard *visited*) b)
                   (setq *open-set*
-                        (sort (cons (cons (manhattan x (* size size)) x) *open-set*) #'< :key #'car))))))
-        (permutation-list p (- size 1)))))
+                        (sort (cons (cons (funcall heuristic x *linear-size*) x) *open-set*) #'< :key #'car))))))
+        (permutation-list p (- *size* 1)))))
 
 
-(defun astar (size)
+(defun astar (heuristic &optional print-path)
   (loop until (null *open-set*)
         for move from 0
         for tupple = (car *open-set*)
         do (if (= 0 (car tupple))
-             (progn (setq *open-set* nil)
+             (let ((path (get-path (p-board (cdr tupple)))))
+              (setq *open-set* nil)
                     (format t "Win with:~T~d moves!~%~T~T~d total opened states~%~T~T~d states tested~%"
-                            (length (get-path (p-board (cdr tupple)))) (hash-table-count *visited*) move))
+                            (length path) (hash-table-count *visited*) move)
+                    (if print-path (mapc (lambda (x) (show-board x)(format t "___~%")) path))
+                    )
              (progn (setq *open-set* (cdr *open-set*))
-                    (get-next-moves (cdr tupple) size)))))
+                    (get-next-moves heuristic (cdr tupple))))))
 
-;recursive looping test
-;(defun astar2 (size moves)
-;  (if (null *open-set*)
-;    (progn (format t "insolvable~%") (return-from astar nil)))
-;  (let ((tupple (car *open-set*)))
-;    (if (= 0 (car tupple))
-;      (format t "Win with:~T~d moves tested!~%~T~T~d total opened states" moves (hash-table-count *visited*))
-;      (progn
-;        (setq *open-set* (cdr *open-set*))
-;        (get-next-moves (cdr tupple) size)
-;        (astar2 size (+ 1 moves))))))
-;
-;(defun init-astar2 (goal start size) ;heuristic?
-;  (setf *goal* goal)
-;  (setf (gethash (p-board start) *visited*) 'end)
-;  (setq *open-set* (cons (cons (manhattan start (* size size)) start) nil))
-;  (astar2 size 0)
-;  )
-
-(defun init-astar (goal start size) ;heuristic?
+(defun init-astar (fn goal start &optional print-path) ;heuristic?
   (setf *goal* goal)
   (setf (gethash (p-board start) *visited*) 'end)
-  (setq *open-set* (cons (cons (manhattan start (* size size)) start) nil))
-  (astar size)
+  (setq *open-set* (cons (cons (funcall fn start *linear-size*) start) nil))
+  (astar fn print-path)
   )
 
 ;make a structure form a list
@@ -84,16 +62,19 @@
                   (set-coord puzzle (nth n lst) i j))))
     puzzle))
 
+(defun  test (fn p siwe)
+  (funcall fn p siwe))
 
 (let* ((p1 (list-to-puzzle '(12 6 8 0 4 10 3 9 14 13 2 15 5 7 1 11) 4))
       (p2 (list-to-puzzle '(1 2 3 4 12 13 14 5 11 0 15 6 10 9 8 7) 4))
-      (p3 (copy-puzzle p1))
       )
-  (show-board (p-board p1))
-  (format t "_________________________~%")
-  (show-board (p-board p2))
-  (format t "_________________________~%")
-  (time (init-astar p2 p1 4))
-  ;(clrhash *visited*)
-  ;(time (init-astar2 p2 p3 4))
+  (setf *size* 4)
+  (setf *linear-size* 16)
+;  (show-board (p-board p1))
+;  (format t "_________________________~%")
+;  (show-board (p-board p2))
+;  (format t "_________________________~%")
+  (time (init-astar #'manhattan p2 p1))
+  (clrhash *visited*)
+  ;(time (init-astar p2 p3 t))
   )
