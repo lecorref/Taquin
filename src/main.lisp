e35cce17364b05bac59b6be1a12c2623a444b531
francois LE CORRE <lecorre.f@gmail.com>
2016-04-23 19:30:42 +0200

    command-line opts

--- src/main.lisp
+++ src/main.lisp
@@ -1,3 +1,13 @@
+(let ((quicklisp-init (merge-pathnames "~/quicklisp/setup.lisp"
+                                       (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
+(with-open-file (*standard-output* "/dev/null" :direction :output
+                                   :if-exists :supersede)
+  (ql:quickload "cl-heap")
+  (ql:quickload "unix-opts"))
+
+(load "src/options.lisp")
 (load "src/algo/solution.lisp")
 (load "src/algo/soluble.lisp")
 (load "src/algo/puzzle.lisp")
@@ -6,13 +16,13 @@
 (load "src/algo/heuristics.lisp")
 (load "src/parser.lisp")
 
-(defun show (start_cells end_cells width &optional show)
-    (setf *size* width)
-    (setf *linear-size* (* width width))
-    (init-astar #'manhattan (list-to-puzzle start_cells width)
-                            (list-to-puzzle end_cells width) show))
+(defun show (start_cells end_cells width heuristic &optional show)
+  (setf *size* width)
+  (setf *linear-size* (* width width))
+  (init-astar heuristic (list-to-puzzle start_cells width)
+              (list-to-puzzle end_cells width) show))
 
-(defun generate-and-solve (width &optional (shuffle 1000) (heuristic #'linear-conflict) show)
+(defun generate-and-solve (width shuffle heuristic &optional show)
   "Generate a random puzzle then solve it
   @args: width:int | &opt: shuffle:int heuristic:function show:bool"
   (setf *size* width)
@@ -23,28 +33,46 @@
     (show-board (p-board new))
     (init-astar heuristic new origin show)))
 
-(defun parse_files (filenames &optional show)
+(defun parse_files (filenames heuristic &optional show)
   (loop for filename in filenames do
-    (let ((fd (open filename :if-does-not-exist nil)))
-      (when fd
-        (let ((width (read_width fd)))
-          (let ((start_cells (read_board width fd))
-                (end_cells (solution width)))
-            (if (eq (is_solvable start_cells end_cells width) 1)
-              (show start_cells end_cells width show)
-              (format t "~d isn't solvable~%" filename)))
-        (close fd))))))
+        (let ((fd (open filename :if-does-not-exist nil)))
+          (when fd
+            (let ((width (read_width fd)))
+              (let ((start_cells (read_board width fd))
+                    (end_cells (solution width)))
+                (if (eq (is_solvable start_cells end_cells width) 1)
+                  (show start_cells end_cells width heuristic show)
+                  (format t "~d isn't solvable~%" filename)))
+              (close fd))))))
 
-(defun main (argv)
-  "Function that resolve the files inputs
-   @args: files:string"
-
-  (if (or (string= (nth 1 argv) "--show")
-          (string= (nth 1 argv) "-s"))
-    (parse_files (subseq argv 2) t))
-    (parse_files (subseq argv 1)))
-
-;(generate-and-solve 9)
-(main *posix-argv*)
+(defun main ()
+  (multiple-value-bind (options free-args)
+    (handler-case
+      (handler-bind ((opts:unknown-option #'unknown-option))
+        (opts:get-opts))
+      (opts:missing-arg (condition)
+                        (format t "fatal: option ~s needs an argument!~%"
+                                (opts:option condition)))
+      (opts:arg-parser-failed (condition)
+                              (format t "fatal: cannot parse ~s as argument of ~s~%"
+                                      (opts:raw-arg condition)
+                                      (opts:option condition)))
+      )
+    (when-option (options :help)
+                 (opts:describe
+                   :prefix "Npuzzle solver"
+                   :suffix "heuristics: manhattan; linear-conflict, misplaced-tiles, n-maxswap"
+                   :usage-of "npuzzle"
+                   :args     "[FREE-ARGS]") (return-from main nil))
+    (when-option (options :generate)
+                   (generate-and-solve it (get-options (options :randomize) 1000)
+                                       (get-options (options :heuristic) #'linear-conflict)
+                                       (getf options :show)))
+    (when-option (options :load)
+                   (parse_files it (get-options (options :heuristic) #'linear-conflict)
+                                       (getf options :show)))
+    )
+  )
+(main)
 ;(declaim (optimize (speed 3) (safety 0) (space 0)))
 ;(sb-ext:save-lisp-and-die "npuzzle" :toplevel #'main :executable t)
